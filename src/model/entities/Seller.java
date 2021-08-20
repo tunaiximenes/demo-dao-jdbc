6a8e3afc49720cd03e7e983a3f7f97d43483185f
Tunai Ximenes <tunai.ximenes@gmail.com>
2021-08-20 03:23:12 -0300

    findById implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -25,6 +25,10 @@ public class Seller implements Serializable {
         this.department = department;
     }
 
+    public Seller() {
+
+    }
+
     public Integer getId() {
         return id;
     }
