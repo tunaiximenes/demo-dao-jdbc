4468a38a2ec20b070df02552e18612b3712522b2
Tunai Ximenes <tunai.ximenes@gmail.com>
2021-08-22 01:08:39 -0300

    Insert Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import javax.naming.SizeLimitExceededException;
 import java.util.Date;
 import java.util.List;
 
@@ -27,6 +28,9 @@ public class Program {
         list = sellerDao.findAll();
         for (Seller obj : list) System.out.println(obj);
 
-
+        System.out.println("\n=== TEST 4: Seller Insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
